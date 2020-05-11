f3748ee694b6ac48de13800c53c8a2308d849b7d
Shiro Han <shan99business@gmail.com>
2020-05-11 22:03:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between(0,8) && !(board[index] == "X" || board[index] == "O")
+  if index.between?(0,8) && !(board[index] == "X" || board[index] == "O")
     true
   else
     false
