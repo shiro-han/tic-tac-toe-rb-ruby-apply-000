24fbd4b4f7a0045b4fe3ef236c8fa44c1310200e
Shiro Han <shan99business@gmail.com>
2020-05-11 22:14:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,6 +53,13 @@ def turn(board)
 end
 
 def turn_count(board)
+  count = 0
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      count += 1
+    end
+  end
+  count
 end
 
 def current_player(board)
