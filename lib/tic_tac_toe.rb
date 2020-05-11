b320848336aae9eb53e8be26a648c76812895f61
Shiro Han <shan99business@gmail.com>
2020-05-11 21:54:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,3 +16,8 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+  index
+end
