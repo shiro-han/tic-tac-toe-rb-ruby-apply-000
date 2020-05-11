c65c3a1c4ea025bc7846bb798d2ef235548eac8e
Shiro Han <shan99business@gmail.com>
2020-05-11 22:09:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,3 +37,17 @@ def valid_move?(board, index)
     false
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
