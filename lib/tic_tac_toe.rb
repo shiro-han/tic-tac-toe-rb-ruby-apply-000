22ee517ce5de5b48a58358cda0e3fb2ed20c7009
Shiro Han <shan99business@gmail.com>
2020-05-11 22:12:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,3 +51,61 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+end
+
+def current_player(board)
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    index1 = win_combo[0]
+    index2 = win_combo[1]
+    index3 = win_combo[2]
+
+    position1 = board[index1]
+    position2 = board[index2]
+    position3 = board[index3]
+
+    array = [position1, position2, position3]
+
+    if array == ["X", "X", "X"] || array == ["O", "O", "O"]
+      return win_combo
+    end
+  end
+  false
+end
+
+def full?(board)
+  all_full = board.none? do |position|
+    position == " "
+  end
+  all_full
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board) == true
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if !(won?(board) == false) || draw?(board) == true || full?(board) == true
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board) == false
+    return nil
+  else
+    win_indexes = won?(board)
+    win_index = win_indexes[0]
+    return board[win_index]
+  end
+end
